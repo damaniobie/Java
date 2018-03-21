a7406235fa4349004429b30ff5aef33bf1d416dd
damaniobie <35574248+damaniobie@users.noreply.github.com>
2018-03-20 21:42:53 -0400

    Update UtopianTree.java

--- HackerRankProblems/UtopianTree.java
+++ HackerRankProblems/UtopianTree.java
@@ -4,6 +4,14 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
+/*
+The Utopian Tree goes through 2 cycles of growth every year. Each spring, it doubles in height. Each summer,
+its height increases by 1 meter.
+
+Laura plants a Utopian Tree sapling with a height of 1 meter at the onset of spring. 
+How tall will her tree be after n growth cycles?
+*/
+
 public class Solution {
 
     static int utopianTree(int n) {
